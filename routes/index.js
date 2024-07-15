4ce50b716cd3ee33f5c7b51e9c4484a722736775
Laplace <lap2837@outlook.com>
2024-07-15 09:57:13 -0600

    had an error with the route /api/users.  fixed

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./api/userRoutes');
 const thoughtRoutes = require('./api/thoughtRoutes');
 
-router.use('/api/users', userRoutes);
-router.use('/api/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
-router.use((req, res) => res.send('Wrong route!'));
+router.use((req, res) => res.status(404).send('Wrong route!'));
 
 module.exports = router;
