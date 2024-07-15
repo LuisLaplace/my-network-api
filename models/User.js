5eada326323717c03eaf43302b67171800bd9003
Laplace <lap2837@outlook.com>
2024-07-15 09:17:51 -0600

    added code to /models/Thought.js and /models/User.js that follow the guidelines for the challenge.

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+// User Schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match a valid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual to get the count of friends
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Initialize the User model
+const User = model('User', userSchema);
+
+module.exports = User;
